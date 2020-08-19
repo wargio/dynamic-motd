3b87ace52e50bbc003d7b1f5549c14a9ee98fd4e
Giovanni Dante Grazioli <giovanni.dantegrazioli@nbs-system.com>
2020-08-19 15:14:16 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+cp -rv update-motd.d/ /etc/
+chmod -v 755 /etc/update-motd.d/
+chmod -v 644 /etc/update-motd.d/colors /etc/update-motd.d/sysinfo.py
+rm -rfv /etc/motd
+ln -sv /var/run/motd /etc/motd
