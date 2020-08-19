dacf4464fba0ff7e711f56fba3e2ec6b10421ab1
Giovanni Dante Grazioli <giovanni.dantegrazioli@nbs-system.com>
2020-08-19 15:20:36 +0200

    remove old install

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ if [ $EUID -ne 0 ]; then
   exit
 fi
 
+if [ -d "/etc/update-motd.d/" ]; then
+	rm -rfv /etc/update-motd.d/
+fi
+
 cp -rv update-motd.d/ /etc/
 chmod -v 755 /etc/update-motd.d/
 chmod -v 644 /etc/update-motd.d/colors /etc/update-motd.d/sysinfo.py
