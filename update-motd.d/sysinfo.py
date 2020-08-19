2e09628a1580affdd1234b40b2c29abac9531401
Giovanni Dante Grazioli <giovanni.dantegrazioli@nbs-system.com>
2020-08-19 14:50:13 +0200

    port to py3

--- update-motd.d/sysinfo.py
+++ update-motd.d/sysinfo.py
@@ -16,8 +16,11 @@
 # Modified by Luc Didry in 2016
 # Get the original version at https://github.com/jnweiger/landscape-sysinfo-mini
 
-import sys,os,time,posix,glob,utmp
-from UTMPCONST import *
+import sys
+import os
+import time
+import glob
+import utmp
 
 _version_ = '1.2'
 
@@ -25,7 +28,8 @@ def utmp_count():
   u = utmp.UtmpRecord()
   users = 0
   for i in u:
-    if i.ut_type == utmp.USER_PROCESS: users += 1
+    if i.ut_type == utmp.USER_PROCESS:
+      users += 1
   return users
 
 def proc_meminfo():
@@ -33,14 +37,13 @@ def proc_meminfo():
   for l in open('/proc/meminfo').readlines():
     a = l.split()
     items[a[0]] = int(a[1])
-  # print items['MemTotal:'], items['MemFree:'], items['SwapTotal:'], items['SwapFree:']
   return items
 
 def proc_mount():
   items = {}
   for m in open('/proc/mounts').readlines():
     a = m.split()
-    if a[0].find('/dev/') is 0:
+    if a[0].find('/dev/') == 0:
       statfs = os.statvfs(a[1])
       perc = 100-100.*statfs.f_bavail/statfs.f_blocks
       gb = statfs.f_bsize*statfs.f_blocks/1024./1024/1024
@@ -51,7 +54,7 @@ def inode_proc_mount():
   items = {}
   for m in open('/proc/mounts').readlines():
     a = m.split()
-    if a[0].find('/dev/') is 0:
+    if a[0].find('/dev/') == 0:
       statfs = os.statvfs(a[1])
       perc = 100-100.*statfs.f_ffree/statfs.f_files
       iTotal = statfs.f_files
@@ -69,28 +72,28 @@ swapperc = "%d%%" % (100-100.*meminfo['SwapFree:']/(meminfo['SwapTotal:'] or 1))
 
 if meminfo['SwapTotal:'] == 0: swapperc = '---'
 
-print "  System information as of %s\n" % time.asctime()
-print "  System load:  %-5.2f                Processes:           %d" % (loadav, processes)
-print "  Memory usage: %-4s                 Users logged in:     %d" % (memperc, users)
-print "  Swap usage:   %s" % (swapperc)
+print ("  System information as of %s\n" % time.asctime())
+print ("  System load:  %-5.2f                Processes:           %d" % (loadav, processes))
+print ("  Memory usage: %-4s                 Users logged in:     %d" % (memperc, users))
+print ("  Swap usage:   %s" % (swapperc))
 
-print "  Disk Usage:"
+print ("  Disk Usage:")
 for k in sorted(statfs.keys()):
-  print "    Usage of %-24s: %-20s" % (k, statfs[k])
+  print ("    Usage of %-24s: %-20s" % (k, statfs[k]))
 
-print "  Inode Usage:"
+print ("  Inode Usage:")
 for l in sorted(iStatfs.keys()):
-  print "    Usage of %-24s: %-20s" % (l, iStatfs[l])
+  print ("    Usage of %-24s: %-20s" % (l, iStatfs[l]))
 
 if users > 0:
     a = utmp.UtmpRecord()
 
-    print "\n  Logged in users:"
+    print ("\n  Logged in users:")
 
     for b in a: # example of using an iterator
-        if b.ut_type == USER_PROCESS:
-            print "  \033[1;31m%-10s\033[m from %-25s at %-20s" % \
-            (b.ut_user, b.ut_host, time.ctime(b.ut_tv[0]))
+        if b.ut_type == utmp.USER_PROCESS:
+            print ("  \033[1;31m%-10s\033[m from %-25s at %-20s" % \
+            (b.ut_user, b.ut_host, time.ctime(b.ut_tv[0])))
     a.endutent()
 
 sys.exit(0)
